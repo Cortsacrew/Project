10fd849d944551105e6021b60ac70d9dcfe32092
Ryan <126432503+Cortsacrew@users.noreply.github.com>
2023-02-26 21:23:24 -0500

    Create StreamingService.java

---
+++ StreamingService.java
@@ -0,0 +1,8 @@
+abstract class StreamingService{
+  public String addOn;
+  public double cost;
+  public int devices;
+  public int subLength;
+  public String sports;
+  abstract void prompt();
+}
